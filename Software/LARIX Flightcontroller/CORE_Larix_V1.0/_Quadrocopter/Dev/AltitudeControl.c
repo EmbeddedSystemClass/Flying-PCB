ae3f59c25744b3123ba69003775656b4a2247405
Mya <448101470@qq.com>
2016-08-28 15:55:35 +0800

    Change baro-related function names and record time when calc alt.

--- Software/LARIX Flightcontroller/CORE_Larix_V1.0/_Quadrocopter/Dev/AltitudeControl.c
+++ Software/LARIX Flightcontroller/CORE_Larix_V1.0/_Quadrocopter/Dev/AltitudeControl.c
@@ -1,24 +1,29 @@
 
-#include "Altitude_Control.h"
+#include "AltitudeControl.h"
+#include "imu.h"
+
+uint32_t baro_alt_update_time;
 
 //# from AP_Baro::get_altitude_difference()
-float Calc_Alt()
+float CalcBaroAlt()
 {
 	float ret;
 	float temp = ground_temperature + 273.15f;
 	float scaling = pressure / ground_pressure;
 	ret = 153.8462f * temp * (1.0f - expf(0.190259f * logf(scaling)));
+	baro_alt_update_time = millis();
 	return ret;
 }
 
-
-void Altitude_Controller()
+void AltitudeController()
 {
 	return;
 }
 
 void Alt_Controller_Int_Handler()
 {
-	alt = Calc_Alt();
+	alt = CalcBaroAlt();
+
+	//CalcEstimatedAlt();
 	alt_ctrl_cnt++;
 }
--- Software/LARIX Flightcontroller/CORE_Larix_V1.0/_Quadrocopter/Dev/AltitudeControl.h
+++ Software/LARIX Flightcontroller/CORE_Larix_V1.0/_Quadrocopter/Dev/AltitudeControl.h
@@ -3,17 +3,22 @@
 #define ALTITUDECONTROL_H_
 
 //#include "def.h"
+#include <stdint.h>		// declare uint8_t
 #include <math.h>
 #include "../Sensors/DPS310/DPS310.h"
+#include "../_HAL/Delay/util.h"
 
 extern float ground_pressure;
 extern float ground_temperature;
 extern float alt;
+extern uint32_t baro_alt_update_time;
+
+extern float vel;
 
 extern int alt_ctrl_cnt;
 
-float Calc_Alt(void);
-void Altitude_Controller(void);
+float CalcBaroAlt(void);
+void AltitudeController(void);
 void Alt_Controller_Int_Handler(void);
 
 #endif /* ALTITUDECONTROL_H_ */
