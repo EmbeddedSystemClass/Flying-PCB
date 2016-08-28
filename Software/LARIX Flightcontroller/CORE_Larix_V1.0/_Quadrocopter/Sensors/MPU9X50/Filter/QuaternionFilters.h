69288ce8bfe3a04134d4f5203739c07250f04d65
Mya <448101470@qq.com>
2016-08-28 17:27:12 +0800

    Attitude estimation: change the PI parameters of MahonyQuaternionUpdate() to get better preciser attitude.

--- Software/LARIX Flightcontroller/CORE_Larix_V1.0/_Quadrocopter/Sensors/MPU9X50/Filter/QuaternionFilters.h
+++ Software/LARIX Flightcontroller/CORE_Larix_V1.0/_Quadrocopter/Sensors/MPU9X50/Filter/QuaternionFilters.h
@@ -12,8 +12,9 @@
 #include <arm_math.h>
 
 #define GYROMEASERROR	PI * (40.0f / 180.0f)
-#define Kp 2.0f * 5.0f // these are the free parameters in the Mahony filter and fusion scheme, Kp for proportional feedback, Ki for integral
-#define Ki 0.0f
+#define Kp 5.0f // these are the free parameters in the Mahony filter and fusion scheme, Kp for proportional feedback, Ki for integral
+#define Ki 0.05f
+//# origin: Kp 2.0f * 5.0f  Ki 0.0f
 
 void MadgwickQuaternionUpdate(float* q, float deltat, float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz);
 void MahonyQuaternionUpdate(float* q, float* eInt, float deltat, float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz);
